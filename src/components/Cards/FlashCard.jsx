01df3677a665158aa67ba78906d4ba15fd6812a4
Rahem <sorourrahem17@gmail.com>
2023-09-29 20:04:59 +0100

    feat(home page): style

--- src/components/Cards/FlashCard.jsx
+++ src/components/Cards/FlashCard.jsx
@@ -1,17 +1,50 @@
 import React from "react"
 import { AiFillThunderbolt } from "react-icons/ai"
 import { TbShoppingCartPlus } from "react-icons/tb"
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs"
 // import Link from "next/link"
 // import { genresList } from "@/utils/static"
 
-const FlashCard = ({ discountPercentage, price, title, thumbnail }) => {
+const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
+  const renderStars = () => {
+    const stars = []
+    const fullStars = Math.floor(rating)
+    const hasHalfStar = rating % 1 !== 0
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(
+        <span key={i} className="text-yellow-400">
+          <BsStarFill />
+        </span>,
+      )
+    }
+
+    if (hasHalfStar && stars.length < 5) {
+      stars.push(
+        <span key={fullStars} className="text-yellow-400">
+          <BsStarHalf />
+        </span>,
+      )
+    }
+
+    while (stars.length < 5) {
+      stars.push(
+        <span key={stars.length} className="text-gray-400">
+          <BsStar />
+        </span>,
+      )
+    }
+
+    return stars
+  }
+
   const calculateDiscountedPrice = (originalPrice, discountPercentage) => {
     const discount = (discountPercentage / 100) * originalPrice
     return (originalPrice - discount).toFixed(2)
   }
 
   return (
-    <div className=" flex-wrap rounded-lg overflow-hidden shadow-lg w-60 h-66 flex-shrink-1 mr-2 ml-2 mb-2 mt-2 relative transform transition-transform duration-300 ease-in-out hover:scale-105">
+    <div className=" flex-wrap rounded-lg overflow-hidden shadow-lg w-60 h-66 flex-shrink-1 mr-2 ml-2 mb-2 relative transform transition-transform duration-300 ease-in-out hover:scale-105">
       <div className="relative">
         <div className="absolute top-0 left-0 bg-red-500 p-2 bg-yellow-500 text-black font-bold text-sm ">
           <AiFillThunderbolt />
@@ -23,6 +56,9 @@ const FlashCard = ({ discountPercentage, price, title, thumbnail }) => {
       <div className="flex flex-col justify-between h-full ">
         <div className=" top-0 left-0 w-full p-3 text-white transition-opacity duration-300 ease-in-out opacity-100">
           <h2 className="text-sm font-bold text-black ">{title}</h2>
+          <p className="my-1 flex list-none gap-0.5 p-0 mt-1">
+            {renderStars()}
+          </p>
           <div className="flex ">
             <span className="font-bold text-red-600 text-sm mt-1 mr-4">
               $ {calculateDiscountedPrice(price, discountPercentage)}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/Cards/ProductCard"
 import FlashCard from "@/components/Cards/FlashCard"
 import { fetcher } from "@/utils/API"
+import { GrNext, GrPrevious } from "react-icons/gr"
 import React, { useState } from "react"
 
 const Homepage = ({ products }) => {
@@ -21,46 +22,42 @@ const Homepage = ({ products }) => {
   const slicedProducts = products.products.slice(0, 5)
 
   return (
-    <main>
-      <div className="image-container relative rounded-large w-full h-80 overflow-hidden">
-        <div className="bg-overlay absolute top-0 left-0 w-full"></div>
-        <img
-          src={product.thumbnail}
-          alt={product.title}
-          // className="w-full h-full rounded-2xl bg-center bg-cover "
-        />
-        <div className="box d_flex top">
-          <div className="side-card">
-            <h1>{product.title}</h1>
-            <p>{product.discountPercentage} % off</p>
+    <main className="bg-gray-100">
+      <div className="relative">
+        {/* Container for the image and buttons */}
+        <div className="relative h-80 overflow-hidden">
+          <div className="bg-overlay absolute top-0 left-0 w-full h-full"></div>
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-full h-full rounded-xl bg-center bg-cover"
+          />
+          <div className="absolute top-1/2 left-0 w-full flex justify-between -translate-y-1/2">
+            <button
+              className="mt-4 rounded-full bg-gray-300 text-white px-2 py-2 ml-4"
+              onClick={previousImage}
+            >
+              {" "}
+              <GrPrevious />
+            </button>
+            <button
+              className="mt-4 rounded-full bg-gray-300 text-white px-2 py-2 mr-4"
+              onClick={nextImage}
+            >
+              <GrNext />
+            </button>
           </div>
         </div>
       </div>
-      <div className="flex justify-between">
-        <button onClick={previousImage}>Previous</button>
-        <button onClick={nextImage}>Next</button>
-      </div>
-      <h1 className="text-2xl font-bold text-YellowPotato flex justify-start p-2">
-        Products you may like
-      </h1>
-      <div className="w-full overflow-hidden flex flex-col flex-wrap md:grid grid-cols-2 lg:flex-row lg:flex ">
-        {slicedProducts.map((product, index) => (
-          <div key={index}>
-            <ProductCard
-              key={index}
-              thumbnail={product.thumbnail}
-              title={product.title}
-              price={product.price}
-              rating={product.rating}
-            />
-          </div>
-        ))}
-      </div>
 
-      <h1 className="text-2xl font-bold text-YellowPotato flex justify-start p-2">
-        Flash Sale
-      </h1>
+      <div className="flex items-center">
+        <h1 className="text-2xl font-semibold text-gray-600 p-2 mt-6 mb-6">
+          Flash Sale
+        </h1>
+        <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
+      </div>
       <div className="w-full overflow-hidden flex flex-col flex-wrap md:grid grid-cols-2 lg:flex-row lg:flex ">
+        {/* <div className="grid grid-cols-5 "> */}
         {slicedProducts
           .filter((product) => product.discountPercentage > 10)
           .map((product, index) => (
@@ -76,34 +73,81 @@ const Homepage = ({ products }) => {
           ))}
       </div>
 
-      <h1 className="text-2xl font-bold text-YellowPotato flex justify-start p-2">
-        Popular Categories
-      </h1>
+      <div className="flex items-center">
+        <h1 className="text-2xl font-semibold text-gray-600 p-2 mt-6 mb-6">
+          Popular Categories
+        </h1>
+        <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 ml-6 mr-6 md:grid-cols-3 grid-cols-1 lg:grid-cols-3">
+        {/* First Column */}
+        <div className="col-span-1 h-80 transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <div className="h-full bg-blue-200 h-80">
+            <img
+              src="Smartphone.png"
+              alt="Image 1"
+              className="w-full h-full mb-1"
+            />
+          </div>
+        </div>
 
-      <div class="grid grid-cols-3 gap-4 h-full ">
-        <div class="col-span-1 w-80">
-          <img src="Smartphone.png" alt="Image 1" class="w-full h-auto " />
+        {/* Second Column */}
+        <div className="col-span-1 h-80">
+          <div className="h-1/2 bg-green-200 mb-2 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <img
+              src="groceries.png"
+              alt="Image 2"
+              className="w-full h-full mb-1"
+            />
+          </div>
+          <div className="h-1/2 bg-green-200 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <img
+              src="perfume.png"
+              alt="Image 5"
+              className="w-full h-full mb-1"
+            />
+          </div>
         </div>
-        <div class="grid grid-col-2 col-span-1 w-80 ">
-          <img
-            src="groceries.png"
-            alt="Image 2"
-            class="w-full h-auto mb-4 col-span-1 "
-          />
-          <img
-            src="skincare.png"
-            alt="Image 3"
-            class="w-full h-auto col-span-1"
-          />
+
+        {/* Third Column */}
+        <div className="col-span-1 h-80">
+          <div className="h-1/2 bg-yellow-200 mb-2 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <img
+              src="laptop.png"
+              alt="Image 4"
+              className="w-full h-full mb-1"
+            />
+          </div>
+          <div className="h-1/2 bg-yellow-200 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <img
+              src="skincare.png"
+              alt="Image 3"
+              className="w-full h-full mb-1"
+            />
+          </div>
         </div>
-        <div class="col-span-1 w-80 h-96 ">
-          <img src="laptop.png" alt="Image 4" class="w-full h-auto mb-4" />
-          <img
-            src="perfume.png"
-            alt="Image 5"
-            class="w-full h-auto object-cover object-center"
-          />
-        </div>
+      </div>
+
+      <div className="flex items-center">
+        <h1 className="text-2xl font-semibold text-gray-600 p-2 mt-6 mb-6">
+          Products you may like
+        </h1>
+        <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
+      </div>
+      <hr className="text-black" />
+      <div className="w-full overflow-hidden flex flex-col flex-wrap md:grid grid-cols-2 lg:flex-row lg:flex ">
+        {/* <div className= "grid grid-cols-5 "> */}
+        {slicedProducts.map((product, index) => (
+          <div key={index}>
+            <ProductCard
+              key={index}
+              thumbnail={product.thumbnail}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+            />
+          </div>
+        ))}
       </div>
     </main>
   )
@@ -113,7 +157,6 @@ export default Homepage
 
 export async function getServerSideProps() {
   const response = await fetcher("products")
-
   return {
     props: {
       products: response,
