319398062e82284bc67619b8d9e924e294676247
Rahem <sorourrahem17@gmail.com>
2023-10-01 21:00:39 +0100

    feat(home page): added carousel, styling and responsives

--- src/components/Cards/FlashCard.jsx
+++ src/components/Cards/FlashCard.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { AiFillThunderbolt } from "react-icons/ai"
 import { TbShoppingCartPlus } from "react-icons/tb"
-import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs"
-// import Link from "next/link"
-// import { genresList } from "@/utils/static"
+import { BsStar } from "react-icons/bs"
+import { FaStar, FaStarHalfAlt } from "react-icons/fa"
 
 const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
   const renderStars = () => {
@@ -14,19 +13,17 @@ const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
     for (let i = 0; i < fullStars; i++) {
       stars.push(
         <span key={i} className="text-yellow-400">
-          <BsStarFill />
+          <FaStar />
         </span>,
       )
     }
-
     if (hasHalfStar && stars.length < 5) {
       stars.push(
         <span key={fullStars} className="text-yellow-400">
-          <BsStarHalf />
+          <FaStarHalfAlt />
         </span>,
       )
     }
-
     while (stars.length < 5) {
       stars.push(
         <span key={stars.length} className="text-gray-400">
@@ -34,7 +31,6 @@ const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
         </span>,
       )
     }
-
     return stars
   }
 
@@ -44,7 +40,7 @@ const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
   }
 
   return (
-    <div className=" flex-wrap rounded-lg overflow-hidden shadow-lg w-60 h-66 flex-shrink-1 mr-2 ml-2 mb-2 relative transform transition-transform duration-300 ease-in-out hover:scale-105">
+    <div className=" grid grid-row-2 rounded-lg overflow-hidden shadow-lg w-full mb-4  transition-transform duration-300 ease-in-out hover:scale-105">
       <div className="relative">
         <div className="absolute top-0 left-0 bg-red-500 p-2 bg-yellow-500 text-black font-bold text-sm ">
           <AiFillThunderbolt />
@@ -55,7 +51,7 @@ const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
 
       <div className="flex flex-col justify-between h-full ">
         <div className=" top-0 left-0 w-full p-3 text-white transition-opacity duration-300 ease-in-out opacity-100">
-          <h2 className="text-sm font-bold text-black ">{title}</h2>
+          <h2 className="text-14px font-bold text-gray-500 ">{title}</h2>
           <p className="my-1 flex list-none gap-0.5 p-0 mt-1">
             {renderStars()}
           </p>
@@ -69,7 +65,7 @@ const FlashCard = ({ rating, discountPercentage, price, title, thumbnail }) => {
           </div>
         </div>
         <div className="px-6 mb-2 flex justify-end">
-          <button className="bg-gray-500 hover:bg-yellow-500 text-white font-semibold px-4 py-1 rounded-lg inline-flex items-center">
+          <button className="bg-blue-500 hover:bg-yellow-500 text-white font-semibold px-4 py-1 rounded-lg inline-flex items-center">
             <TbShoppingCartPlus className="mr-1" />
             Add
           </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,57 @@
 import ProductCard from "@/components/Cards/ProductCard"
 import FlashCard from "@/components/Cards/FlashCard"
 import { fetcher } from "@/utils/API"
-import { GrNext, GrPrevious } from "react-icons/gr"
-import React, { useState } from "react"
+import React from "react"
 import Navbar from "@/components/Navbar/Navbar"
+import Carousel from "@/components/Carousel"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const Homepage = ({ products }) => {
   console.log(products.products)
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
-  const nextImage = () => {
-    setCurrentImageIndex(
-      (prevIndex) => (prevIndex + 1) % products.products.length,
-    )
-  }
-  const previousImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? products.products.length - 1 : prevIndex - 1,
-    )
-  }
-  const product = products.products[currentImageIndex]
   const slicedProducts = products.products.slice(0, 5)
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  }
 
   return (
     <div>
       <Navbar />
-
       <main className="bg-gray-100">
-        <div className="relative">
-          {/* Container for the image and buttons */}
-          <div className="relative h-80 overflow-hidden">
-            <div className="bg-overlay absolute top-0 left-0 w-full h-full"></div>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-              className="w-full h-full rounded-xl bg-center bg-cover"
-            />
-            <div className="absolute top-1/2 left-0 w-full flex justify-between -translate-y-1/2">
-              <button
-                className="mt-4 rounded-full bg-gray-300 text-white px-2 py-2 ml-4"
-                onClick={previousImage}
-              >
-                {" "}
-                <GrPrevious />
-              </button>
-              <button
-                className="mt-4 rounded-full bg-gray-300 text-white px-2 py-2 mr-4"
-                onClick={nextImage}
-              >
-                <GrNext />
-              </button>
-            </div>
+        <div className="grid grid-cols-3 gap-4 pt-6 lg:pt-0">
+          <div className="col-span-3">
+            <Slider {...settings}>
+              {products.products.map((product, index) => (
+                <Carousel
+                  key={product.id}
+                  thumbnail={product.thumbnail}
+                  title={product.title}
+                  price={product.price}
+                />
+              ))}
+            </Slider>
           </div>
         </div>
 
@@ -60,8 +61,7 @@ const Homepage = ({ products }) => {
           </h1>
           <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
         </div>
-        <div className="w-full overflow-hidden flex flex-col flex-wrap md:grid grid-cols-2 lg:flex-row lg:flex ">
-          {/* <div className="grid grid-cols-5 "> */}
+        <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
           {slicedProducts
             .filter((product) => product.discountPercentage > 10)
             .map((product, index) => (
@@ -83,50 +83,44 @@ const Homepage = ({ products }) => {
           </h1>
           <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
         </div>
-        <div className="grid grid-cols-3 gap-4 ml-6 mr-6 md:grid-cols-3 grid-cols-1 lg:grid-cols-3">
+
+        <div className="grid grid-cols-1 gap-4 ml-6 mr-6 md:grid-cols-2 lg:grid-cols-3">
           {/* First Column */}
-          <div className="col-span-1 h-80 transform transition-transform duration-300 ease-in-out hover:scale-105">
-            <div className="h-full bg-blue-200 h-80">
+          <div className="col-span-1 grid-col-1 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <div className="col-span-1 bg-blue-200 rounded-lg">
               <img
                 src="Smartphone.png"
                 alt="Image 1"
-                className="w-full h-full mb-1"
+                className="w-full h-full object-cover rounded-lg"
               />
             </div>
           </div>
 
           {/* Second Column */}
-          <div className="col-span-1 h-80">
-            <div className="h-1/2 bg-green-200 mb-2 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <div className=" grid grid-col-2 col-span-1 gap-4">
+            <div className="col-span-1 bg-green-200 mb-2 rounded-lg transform transition-transform duration-300 ease-in-out hover:scale-105">
               <img
                 src="groceries.png"
                 alt="Image 2"
-                className="w-full h-full mb-1"
+                className="w-full h-full object-cover rounded-lg"
               />
             </div>
-            <div className="h-1/2 bg-green-200 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+            <div className="col-span-1 bg-green-200 rounded-lg transform transition-transform duration-300 ease-in-out hover:scale-105">
               <img
                 src="perfume.png"
                 alt="Image 5"
-                className="w-full h-full mb-1"
+                className="w-full h-full object-cover rounded-lg"
               />
             </div>
           </div>
 
           {/* Third Column */}
-          <div className="col-span-1 h-80">
-            <div className="h-1/2 bg-yellow-200 mb-2 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
-              <img
-                src="laptop.png"
-                alt="Image 4"
-                className="w-full h-full mb-1"
-              />
-            </div>
-            <div className="h-1/2 bg-yellow-200 h-40 transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <div className="grid grid-col-1 col-span-1 gap-4">
+            <div className="col-span-1 h-full rounded-lg transform transition-transform duration-300 ease-in-out hover:scale-105">
               <img
                 src="skincare.png"
                 alt="Image 3"
-                className="w-full h-full mb-1"
+                className="w-full h-full object-center object-cover rounded-lg"
               />
             </div>
           </div>
@@ -138,9 +132,7 @@ const Homepage = ({ products }) => {
           </h1>
           <hr className="my-4 mx-2 border-t border-gray-400 flex-grow" />
         </div>
-        <hr className="text-black" />
-        <div className="w-full overflow-hidden flex flex-col flex-wrap md:grid grid-cols-2 lg:flex-row lg:flex ">
-          {/* <div className= "grid grid-cols-5 "> */}
+        <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
           {slicedProducts.map((product, index) => (
             <div key={index}>
               <ProductCard
