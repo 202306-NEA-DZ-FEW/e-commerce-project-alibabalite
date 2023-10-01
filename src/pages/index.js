ccd391c71acd5419e43896007b59e3d6eaa00f26
Farouk <faroukwebdesign@gmail.com>
2023-10-01 18:16:40 +0100

    style(footer): create basic style and functionality of footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import Image from "next/image"
 import { fetcher } from "@/utils/API"
+import Footer from "@/components/Footer/Footer"
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <Footer />
+    </main>
+  )
 }
